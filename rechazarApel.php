7a190a7f7cbe3015787c3d3189ad04d20f4019d6
Dec1mate <38561257+Dec1mate@users.noreply.github.com>
2019-06-10 13:12:54 +0200

    Delete rechazarApel.php

--- rechazarApel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "Database/Connection.php";
-
-$id=$_GET['id'];
-$PDO = Connection::Make();
-$result=$PDO->prepare("update apelaciones set estado=1 where id=?");
-$result->execute([$id]);
-
-header("location: administrarApelaciones.php");
