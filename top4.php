f66976014a165b6709d8fdcf63b8cdbed2481bca
Dec1mate <38561257+Dec1mate@users.noreply.github.com>
2019-06-10 13:12:20 +0200

    Delete top4.php

--- top4.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$PDO = Connection::Make();
-$result=$PDO->prepare("select * from conduce c, rutas r, coches co where c.estado !=0 AND r.nombre=c.nombre AND co.matricula=c.matricula order by fecha_conduce LIMIT 2");
-$result->execute();
-$top4=$result->fetchAll(PDO::FETCH_ASSOC);
-$top4=json_encode($top4);
-$top4=json_decode($top4);
-?>
