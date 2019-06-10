5833ed0936d79439bc9e8531064f99fe2492b9da
Dec1mate <38561257+Dec1mate@users.noreply.github.com>
2019-06-10 13:13:21 +0200

    Delete logoff.php

--- logoff.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include ('./chat/Chat.php');
-$chat = new Chat();
-$chat->updateUserOnline($_SESSION['userid'], 0);
-
-session_destroy();
-header("location: index.php");
-?>
